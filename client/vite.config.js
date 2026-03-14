0a57c463877e35af2dd8d69f2c16cb84fb1f2d52
Carlos Jael Guillen Gonzalez <gcarlosjael@gmail.com>
2026-03-13 22:02:33 -0600

    Trying again to add 404 to github pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
